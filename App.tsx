b7e8efa118fd583adeabe7f7e6879b5628135da9
fluorjo <fluorjo@naver.com>
2024-08-02 09:00:10 +0900

    버튼으로 소리 재생, 정지 성공.

--- App.tsx
+++ App.tsx
@@ -1,14 +1,68 @@
-import {StyleSheet, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
 
+import {Button, StyleSheet, View} from 'react-native';
+
+import Sound from 'react-native-sound';
+Sound.setCategory('Playback');
+var myRemoteSound = new Sound(
+  'https://www.soundjay.com/ambient/sounds/boarding-accouncement-1.mp3',
+  null,
+  error => {
+    if (error) {
+      console.log('failed to load the sound', error);
+      return;
+    } else {
+      myRemoteSound.play(success => {
+        if (success) {
+          console.log('Sound playing');
+        } else {
+          console.log('Issue playing file');
+          console.log(error);
+        }
+      });
+    }
+  },
+);
 export default function App() {
-  // eslint-disable-next-line react/react-in-jsx-scope
-  return <View style={styles.container} />;
+  const [playing, setPlaying] = useState(false); // 초기값을 false로 설정
+
+  useEffect(() => {
+    myRemoteSound.setVolume(1);
+    return () => {
+      myRemoteSound.release();
+    };
+  }, []);
+  const playPause = () => {
+    if (myRemoteSound.isPlaying()) {
+      myRemoteSound.pause();
+      setPlaying(false);
+    } else {
+      setPlaying(true);
+      myRemoteSound.play(success => {
+        if (success) {
+          setPlaying(false);
+
+          console.log('Sound playing');
+        } else {
+          setPlaying(false);
+          console.log('Issue playing file');
+          console.log(Error);
+        }
+      });
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Button title="beep" onPress={playPause} />
+    </View>
+  );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
 });
---
+++ memo.md
@@ -0,0 +1,4 @@
+https://freesound.org/people/ramicio/packs/9796/
+https://simpleguics2pygame.readthedocs.io/en/latest/_static/links/snd_links.html
+
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home
